5b16c57aa5c8dd3a7c4c1dc9cfa723db3444b611
Sagar <63837795+sagar-source@users.noreply.github.com>
2020-04-24 13:25:00 +0530

    Change in flickr api

--- app/src/main/java/com/example/coolassignment/API/FlickrAPI.kt
+++ app/src/main/java/com/example/coolassignment/API/FlickrAPI.kt
@@ -2,5 +2,5 @@ package com.example.coolassignment.API
 
 object FlickrAPI {
 
-    var url = "https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=bf91986ff941d47074993c154eb752f2&format=json&nojsoncallback=1"
+    var url = "https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=37fbaf24752e5e132e6bed51a0663516&format=json&nojsoncallback=1&api_sig=f248c521564e58c85dde7f5bb0f17b53"
 }
