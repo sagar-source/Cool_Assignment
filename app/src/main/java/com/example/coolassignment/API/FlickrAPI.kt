9782186c9360e714cc3d255d7ad6eed6d9cdf98f
Sagar <a_Sagar.gugnani@airtelbank.com>
2020-04-25 09:48:32 +0530

    API Key changed

--- app/src/main/java/com/example/coolassignment/API/FlickrAPI.kt
+++ app/src/main/java/com/example/coolassignment/API/FlickrAPI.kt
@@ -2,5 +2,5 @@ package com.example.coolassignment.API
 
 object FlickrAPI {
 
-    var url = "https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=37fbaf24752e5e132e6bed51a0663516&format=json&nojsoncallback=1&api_sig=f248c521564e58c85dde7f5bb0f17b53"
+    var url = "https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=915c25e97a978930e83506d9254a405c&format=json&nojsoncallback=1"
 }
